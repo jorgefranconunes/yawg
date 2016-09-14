78b2c3a4e4aef228961f24e36cdd87b47124216c
jorgefranconunes <jorgefranconunes@gmail.com>
2016-09-14 20:18:32 +0100

    Refs #187 Moved yawg specific template data to dedicated workspace

--- modules/core/src/main/java/com/varmateo/yawg/freemarker/FreemarkerDataModel.java
+++ modules/core/src/main/java/com/varmateo/yawg/freemarker/FreemarkerDataModel.java
@@ -16,6 +16,9 @@ public final class FreemarkerDataModel
         extends Object {
 
 
+    private static final String KEY_YAWG_DATA = "yawg";
+
+
     private final TemplateDataModel _data;
 
 
@@ -31,72 +34,12 @@ public final class FreemarkerDataModel
     /**
      *
      */
-    public String getBakeId() {
-
-        return _data.getBakeId();
-    }
-
-
-    /**
-     *
-     */
-    public String getBody() {
-
-        return _data.getBody();
-    }
-
-
-    /**
-     *
-     */
-    public String getPageUrl() {
-
-        return _data.getPageUrl();
-    }
-
-
-    /**
-     *
-     */
-    public String getProductName() {
-
-        return _data.getProductName();
-    }
-
-
-    /**
-     *
-     */
-    public String getRootRelativeUrl() {
-
-        return _data.getRootRelativeUrl();
-    }
-
-
-    /**
-     *
-     */
-    public String getTitle() {
-
-        return _data.getTitle();
-    }
-
-
-    /**
-     *
-     */
-    public String getVersion() {
-
-        return _data.getVersion();
-    }
-
-
-    /**
-     *
-     */
     public Object get(final String key) {
 
-        Object result = _data.getPageVars().get(key).orElse(null);
+        Object result =
+                KEY_YAWG_DATA.equals(key)
+                ? _data
+                : _data.getPageVars().get(key).orElse(null);
 
         return result;
     }
